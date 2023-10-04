503329af34aafd0a9a92a1fd8f7a6a342db05ca4
masum khan <study.contents.of.masumkhan@gmail.com>
2023-10-05 02:23:44 +0600

    starter pack for redux based frontend

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className=" bg-slate-300 text-green-600">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
